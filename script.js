0bf4726bb8210212196a6f302a2cafdb0571f1f8
Renan Rodrigo <renanrb@ime.usp.br>
2018-07-25 09:06:33 -0300

    Alternar entre X e O

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const casas = document.querySelectorAll(".casa");
+let contador = 0;
 
 for(let casa of casas){
     casa.onclick = clicar;
 }
 
 function clicar(){
-    this.innerHTML = "X";
-    this.style.color = "blue";
+    contador++;
+
+    if(contador % 2){
+        this.innerHTML = "X";
+        this.style.color = "blue";
+    }
+    else{
+        this.innerHTML = "O";
+        this.style.color = "green";
+    }
 }
